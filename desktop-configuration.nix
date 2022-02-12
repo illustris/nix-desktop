8c1e038f2e52cb30adbbab38d7de2ad34a97a23b
illustris <rharikrishnan95@gmail.com>
2022-02-12 20:26:32 +0530

    bump

--- desktop-configuration.nix
+++ desktop-configuration.nix
@@ -84,28 +84,7 @@
 
 	nixpkgs.config.allowUnfree = true;
 	programs.steam.enable = true;
-	programs.chromium = {
-		enable = true;
-		extensions = [
-			"gcbommkclmclpchllfjekcdonpmejbdp" # https everywhere
-			"cjpalhdlnbpafiamejdnhcphjbkeiagm" # ublock origin
-			"iipjdmnoigaobkamfhnojmglcdbnfaaf" # Clutter Free
-			"bdakmnplckeopfghnlpocafcepegjeap" # RescueTime
-			"gppongmhjkpfnbhagpmjfkannfbllamg" # wappalyzer
-			"lcbjdhceifofjlpecfpeimnnphbcjgnc" # xBrowserSync
-			"chlffgpmiacpedhhbkiomidkjlcfhogd" # pushbullet
-			"aghfnjkcakhmadgdomlmlhhaocbkloab" # just black
-			"fmkadmapgofadopljbjfkapdkoienihi" # React Developer Tools
-		];
-		homepageLocation = "https://sx.illustris.tech/";
-		defaultSearchProviderSuggestURL = "https://sx.illustris.tech/autocompleter?q={searchTerms}";
-		defaultSearchProviderSearchURL = "https://sx.illustris.tech/search?q={searchTerms}";
-		extraOpts = {
-			DefaultSearchProviderEnabled = true;
-			DefaultSearchProviderName = "Sx";
-			DefaultCookiesSetting = 1;
-		};
-	};
+
 	environment.systemPackages = with pkgs; [
 		st
 		dmenu
